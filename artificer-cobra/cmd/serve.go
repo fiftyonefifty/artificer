d59c1b4eea49eafbbd1f5259f771f51ae13c3731
Herb Stahl <ghstahl@gmail.com>
2019-10-31 10:18:39 -0700

    initial cobrafied version of artificer

---
+++ artificer-cobra/cmd/serve.go
@@ -0,0 +1,372 @@
+/*
+Copyright © 2019 Artificer Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"artificer/pkg/api/handlers"
+	"artificer/pkg/client/loaders"
+	"artificer/pkg/config"
+	"artificer/pkg/keyvault"
+	"artificer/pkg/util"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	echo "github.com/labstack/echo/v4"
+	middleware "github.com/labstack/echo/v4/middleware"
+	"github.com/robfig/cron/v3"
+	"github.com/spf13/viper"
+)
+
+var (
+	ProcessDirectory string
+)
+
+func init() {
+	var err error
+	ProcessDirectory, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ProcessDirectory)
+
+	viper.SetConfigFile("config/appsettings.json")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	useKeyVault := viper.GetBool("clientConfig.useKeyVault") // true|false
+
+	loaders.InitializeClientConfig(loaders.ClientConfigOptions{
+		RootFolder:  ProcessDirectory,
+		UseKeyVault: useKeyVault,
+	})
+}
+
+func Alive() {
+	now := time.Now().UTC()
+	fmt.Println(fmt.Sprintf("Alive:%s", now))
+}
+
+func executeEcho() {
+
+	var err error
+
+	keyVaultDone := make(chan bool, 1)
+	clientConfigDone := make(chan bool, 1)
+	go func() {
+		fmt.Println("Startup Enter ... DoKeyvaultBackground")
+		keyvault.DoKeyvaultBackground()
+		fmt.Println("Startup Complete ... DoKeyvaultBackground")
+		keyVaultDone <- true
+	}()
+	go func() {
+		var (
+			ctx    context.Context
+			cancel context.CancelFunc
+		)
+		ctx, cancel = context.WithCancel(context.Background())
+		defer cancel() // Cancel ctx as soon as handleSearch returns.
+
+		fmt.Println("Startup Enter ... LoadClientConfig")
+		loaders.LoadClientConfig(ctx)
+		fmt.Println("Startup Complete ... LoadClientConfig")
+		clientConfigDone <- true
+	}()
+
+	c := cron.New()
+	cronSpec := viper.GetString("keyVault.cronSpec") // i.e. "@every 10s"
+	_, err = c.AddFunc(cronSpec, func() {
+		fmt.Println("CRON Enter ... DoKeyvaultBackground")
+		keyvault.DoKeyvaultBackground()
+		fmt.Println("CRON Complete ... DoKeyvaultBackground")
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	cronSpec = viper.GetString("clientConfig.cronSpec") // i.e. "@every 5min"
+
+	_, err = c.AddFunc(cronSpec, func() {
+		var (
+			ctx    context.Context
+			cancel context.CancelFunc
+		)
+		ctx, cancel = context.WithCancel(context.Background())
+		defer cancel() // Cancel ctx as soon as handleSearch returns.
+
+		fmt.Println("CRON Enter ... LoadClientConfig")
+		loaders.LoadClientConfig(ctx)
+		fmt.Println("CRON Complete ... LoadClientConfig")
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	c.Start()
+
+	// Creating a new Echo instance.
+	e := echo.New()
+	// Configure Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost},
+	}))
+
+	//e.File("/", "static/index.html")
+
+	// in order to serve static assets
+	//e.Static("/static", "static")
+
+	// Route / to handler function
+	e.GET("/", handlers.Index)
+	e.GET("/health", handlers.HealthCheck)
+	e.GET("/.well-known/openid-configuration", handlers.WellKnownOpenidConfiguration)
+	e.GET("/.well-known/openid-configuration/jwks", handlers.WellKnownOpenidConfigurationJwks)
+	e.GET("/mint-test-token", handlers.MintTestToken)
+	e.POST("/connect/token", handlers.TokenEndpoint)
+	e.GET("/get-test-secret", handlers.GetTestSecret)
+	// V1 Routes
+	// v1 := e.Group("/v1")
+	// v1Tokens := v1.Group("/tokens")
+	// v1Tokens.GET("/tokens", handlers)
+	fmt.Println()
+	fmt.Println("------ Waiting for initial go routines to complete ------")
+	fmt.Println()
+
+	allDoneChannel := util.WaitOnAllChannels(keyVaultDone, clientConfigDone)
+	<-allDoneChannel
+
+	// If you start me up, I'll never stop
+	e.Logger.Fatal(e.Start(":9000"))
+}
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+		sc, err := validateVehicleRequest(cmd)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		config.SetGroupName(sc.AzureGroupName)
+		config.SetTenantID(sc.AzureTenantId)
+		config.SetSubscriptionID(sc.AzureSubscriptionId)
+		config.SetClientID(sc.KeyVaultClientId)
+		config.SetClientSecret(sc.KeyVaultClientSecret)
+
+		executeEcho()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	var (
+		cliFlags *cliFlags
+	)
+	sc := ServerConfig{}
+
+	cliFlags = sc.CliFlags_KeyVaultClientId()
+	addFlag(serveCmd, cliFlags)
+
+	cliFlags = sc.CliFlags_KeyVaultClientSecret()
+	addFlag(serveCmd, cliFlags)
+
+	cliFlags = sc.CliFlags_AzureGroupName()
+	addFlag(serveCmd, cliFlags)
+
+	cliFlags = sc.CliFlags_AzureSubscriptionId()
+	addFlag(serveCmd, cliFlags)
+
+	cliFlags = sc.CliFlags_AzureTenantId()
+	addFlag(serveCmd, cliFlags)
+
+	cliFlags = sc.CliFlags_Port()
+	addFlag(serveCmd, cliFlags)
+}
+
+func validateVehicleRequest(cmd *cobra.Command) (sc *ServerConfig, err error) {
+	var (
+		val string
+	)
+	sc = &ServerConfig{}
+
+	val = viper.GetString("AF-key-vault-client-id")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("key-vault-client-id")
+		if err != nil {
+			return
+		}
+		sc.KeyVaultClientId = val
+	}
+
+	val = viper.GetString("AF-key-vault-client-secret")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("key-vault-client-secret")
+		if err != nil {
+			return
+		}
+		sc.KeyVaultClientSecret = val
+	}
+
+	val = viper.GetString("AF-az-group-name")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("az-group-name")
+		if err != nil {
+			return
+		}
+		sc.AzureGroupName = val
+	}
+
+	val = viper.GetString("AF-az-subscription-id")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("az-subscription-id")
+		if err != nil {
+			return
+		}
+		sc.AzureSubscriptionId = val
+	}
+
+	val = viper.GetString("AF-az-tenant-id")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("az-tenant-id")
+		if err != nil {
+			return
+		}
+		sc.AzureTenantId = val
+	}
+
+	val = viper.GetString("AF-port")
+	if len(val) == 0 {
+		val, err = cmd.Flags().GetString("port")
+		if err != nil {
+			return
+		}
+		sc.Port = val
+	}
+
+	return
+}
+
+var (
+	_ServerConfigType                reflect.Type
+	_KeyVaultClientIdStructField     reflect.StructField
+	_KeyVaultClientSecretStructField reflect.StructField
+	_AzureGroupNameStructField       reflect.StructField
+	_AzureSubscriptionIdStructField  reflect.StructField
+	_AzureTenantIdStructField        reflect.StructField
+	_PortStructField                 reflect.StructField
+)
+
+type ServerConfig struct {
+	KeyVaultClientId     string `cli-required:"true" cli-long:"key-vault-client-id" cli-short:"" cli-default:"" cli-description:"Azure KeyVault Client Id" validate:"gt=1  & format=alnum_unicode"`
+	KeyVaultClientSecret string `cli-required:"true" cli-long:"key-vault-client-secret" cli-short:"" cli-default:"" cli-description:"Azure KeyVault Client Secret" validate:"gt=1  & format=alnum_unicode"`
+	AzureGroupName       string `cli-required:"true" cli-long:"az-group-name" cli-short:"" cli-default:"" cli-description:"Azure Group Name" validate:"gt=1  & format=alnum_unicode"`
+	AzureSubscriptionId  string `cli-required:"true" cli-long:"az-subscription-id" cli-short:"" cli-default:"" cli-description:"Azure Subscription Id" validate:"gt=1  & format=alnum_unicode"`
+	AzureTenantId        string `cli-required:"true" cli-long:"az-tenant-id" cli-short:"" cli-default:"" cli-description:"Azure Tenant Id" validate:"gt=1  & format=alnum_unicode"`
+	Port                 string `cli-required:"false" cli-long:"port" cli-short:"p" cli-default:"" cli-description:"Artifice Server Port" validate:"gt=1  & format=alnum_unicode"`
+}
+
+func buildServerConfigReflectionData() {
+	if _ServerConfigType == nil {
+		_ServerConfigType = reflect.TypeOf(ServerConfig{})
+		_KeyVaultClientIdStructField, _ = _ServerConfigType.FieldByName("KeyVaultClientId")
+		_KeyVaultClientSecretStructField, _ = _ServerConfigType.FieldByName("KeyVaultClientSecret")
+		_AzureGroupNameStructField, _ = _ServerConfigType.FieldByName("AzureGroupName")
+		_AzureSubscriptionIdStructField, _ = _ServerConfigType.FieldByName("AzureSubscriptionId")
+		_AzureTenantIdStructField, _ = _ServerConfigType.FieldByName("AzureTenantId")
+		_PortStructField, _ = _ServerConfigType.FieldByName("Port")
+	}
+}
+func (m *ServerConfig) CliFlags_KeyVaultClientId() *cliFlags {
+	return m.getWellknownCliFlags(&_KeyVaultClientIdStructField)
+}
+func (m *ServerConfig) CliFlags_KeyVaultClientSecret() *cliFlags {
+	return m.getWellknownCliFlags(&_KeyVaultClientSecretStructField)
+}
+func (m *ServerConfig) CliFlags_AzureGroupName() *cliFlags {
+	return m.getWellknownCliFlags(&_AzureGroupNameStructField)
+}
+func (m *ServerConfig) CliFlags_AzureSubscriptionId() *cliFlags {
+	return m.getWellknownCliFlags(&_AzureSubscriptionIdStructField)
+}
+func (m *ServerConfig) CliFlags_AzureTenantId() *cliFlags {
+	return m.getWellknownCliFlags(&_AzureTenantIdStructField)
+}
+func (m *ServerConfig) CliFlags_Port() *cliFlags {
+	return m.getWellknownCliFlags(&_PortStructField)
+}
+func (m *ServerConfig) getWellknownCliFlags(sf *reflect.StructField) *cliFlags {
+	buildServerConfigReflectionData()
+	cliLong, _ := sf.Tag.Lookup("cli-long")
+	cliShort, _ := sf.Tag.Lookup("cli-short")
+	cliDescription, _ := sf.Tag.Lookup("cli-description")
+	cliDefault, _ := sf.Tag.Lookup("cli-default")
+	cliRequired, _ := sf.Tag.Lookup("cli-required")
+	required, err := strconv.ParseBool(cliRequired)
+	if err != nil {
+		panic(err)
+	}
+
+	return &cliFlags{
+		LongFlag:    cliLong,
+		ShortFlag:   cliShort,
+		Description: cliDescription,
+		Default:     cliDefault,
+		Required:    required,
+	}
+}
+func addFlag(cmd *cobra.Command, cliFlags *cliFlags) {
+	if len(cliFlags.ShortFlag) == 1 {
+		cmd.Flags().StringP(cliFlags.LongFlag, cliFlags.ShortFlag, cliFlags.Default, cliFlags.Description)
+	} else {
+		cmd.Flags().String(cliFlags.LongFlag, cliFlags.Default, cliFlags.Description)
+
+	}
+	if cliFlags.Required {
+		cobra.MarkFlagRequired(cmd.Flags(), cliFlags.LongFlag)
+	}
+}
--- go.mod
+++ go.mod
@@ -40,9 +40,10 @@ require (
 	github.com/marstr/randname v0.0.0-20181206212954-d5b0f288ab8c
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/pascaldekloe/jwt v1.6.1-0.20191011205700-9118cfd8bc6d
-	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pmylund/go-cache v2.1.0+incompatible
 	github.com/prometheus/client_golang v1.1.0 // indirect
 	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
@@ -54,6 +55,7 @@ require (
 	github.com/rogpeppe/go-internal v1.4.0 // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cobra v0.0.5
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.4.0
@@ -73,7 +75,7 @@ require (
 	golang.org/x/mobile v0.0.0-20191002175909-6d0d39b2ca82 // indirect
 	golang.org/x/net v0.0.0-20191021144547-ec77196f6094 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20191022100944-742c48ecaeb7 // indirect
+	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
 	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
 	golang.org/x/tools v0.0.0-20191003162220-c56b4b191e2d // indirect
 	google.golang.org/api v0.10.0 // indirect
@@ -82,5 +84,4 @@ require (
 	google.golang.org/grpc v1.24.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/square/go-jose.v2 v2.3.1
-	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
